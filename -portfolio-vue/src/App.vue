8600092afb2d3af2c902f65ef0ba10fa2dc3348c
Eunice Sarah Siregar <13521013@mahasiswa.itb.ac.id>
2024-07-28 21:00:19 +0700

    feat: navbar logic

--- -portfolio-vue/src/App.vue
+++ -portfolio-vue/src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue';
 import Projects from './components/Projects.vue';
 import Greeting from './components/Greeting.vue';
 import Contact from './components/Contact.vue';
 import AboutMe from './components/AboutMe.vue';
 import Experiences from './components/Experiences.vue';
+import { onMounted, ref } from "vue";
+import AOS from "aos";
+import "aos/dist/aos.css"; // Ensure you import the AOS CSS
+onMounted(() => {
+    AOS.init();
+  });
 </script>
 
 <template>
--- -portfolio-vue/src/router/index.ts
+++ -portfolio-vue/src/router/index.ts
@@ -1,22 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import AboutMe from '@/components/AboutMe.vue';
+import Contact from '@/components/Contact.vue';
+import Experiences from '@/components/Experiences.vue';
+import Greeting from '@/components/Greeting.vue';
+import Projects from '@/components/Projects.vue';
+
+const routes = [
+  { path: '/', redirect: '/greeting' },
+  { path: '/greeting', name: 'greeting', component: Greeting },
+  { path: '/contact', name: 'contact', component: Contact },
+  { path: '/aboutme', name: 'aboutme', component: AboutMe },
+  { path: '/experiences', name: 'experiences', component: Experiences },
+  { path: '/projects', name: 'projects', component: Projects }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import()
-    // }
-  ]
-})
+  routes
+});
 
-export default router
+export default router;
